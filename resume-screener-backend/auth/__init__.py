f91d6462a8ecf710086c69c157d2b53eddbec9b5
Jaswanth-1704 <jazzsourcecode@gmail.com>
2025-04-05 21:49:08 +0530

    jazz

--- resume-screener-backend/auth/__init__.py
+++
@@ -1,6 +0,0 @@
-
-# This file is required to make the directory a package
-from auth.routes import init_jwt
-
-def init_app(app):
-    init_jwt(app)
--- resume-screener-backend/hr/__init__.py
+++
@@ -1,2 +0,0 @@
-
-# This file is required to make the directory a package
